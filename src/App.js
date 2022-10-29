fc7eccf69c03bc918e79989430b04890821e2c0a
Vinicius Viana <viniciuspro333@gmail.com>
2022-10-29 15:37:08 -0300

    Localizador React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './style.css';
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+
+
+  async function handleSearch(){
+    
+
+    if(input === ""){
+      alert('preencha com o CEP')
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput("")
+
+    }catch{
+      alert("erro ao buscar");
+      setInput("")
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="caixa">
+      <h1 classNmae="title">Localizador</h1>
+
+      <div className="caixaInput">
+        <input 
+        type="text" 
+        placeholder="digite o CEP"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>Procurar</button>
+      </div>
+      <div className="main">
+        <h2>CEP: {cep.cep}</h2>
+        <span>Rua: {cep.logradouro}</span>
+        <span>Complemento: {cep.complemento}</span>
+        <span>{cep.bairro}</span>
+        <span>{cep.localidade}- {cep.uf}</span>
+      </div>
     </div>
   );
 }
 
 export default App;
+
